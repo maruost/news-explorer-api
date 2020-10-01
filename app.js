2a273a8b2b533cae2452d804bd87e2dc89bdbb8a
unknown <ostapenko.m.a@yandex.ru>
2020-10-01 13:17:08 +0300

    joi validation added, errors handlers upd

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { errors, celebrate, Joi } = require('celebrate');
 
 const auth = require('./middlewares/auth');
 
 const users = require('./routes/users');
 const articles = require('./routes/articles');
 const { login, createUser } = require('./controllers/users');
+const NotFoundError = require('./helpers/not-found-error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -23,10 +25,42 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().trim().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
 app.use('/users', auth, users);
 app.use('/articles', auth, articles);
+
+app.use((req, res) => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
