d820d8fc708d076479244a6a068f39d80e253e86
unknown <ostapenko.m.a@yandex.ru>
2020-10-31 22:25:24 +0300

    cookies same site

--- controllers/users.js
+++ controllers/users.js
@@ -61,7 +61,7 @@ module.exports.login = (req, res, next) => {
         .cookie("jwt", token, {
           maxAge: 3600000,
           httpOnly: true,
-          sameSite: true,
+          // sameSite: true,
         })
         .send({ message: infoMessages.auth });
     })
