3ab4a9c7fe02b9411924c7a237630c5494d295f9
unknown <ostapenko.m.a@yandex.ru>
2020-10-31 22:19:07 +0300

    cors

--- helpers/config.js
+++ helpers/config.js
@@ -7,6 +7,7 @@ module.exports.mongoAdress = "mongodb://localhost:27017/newsdb";
 //cors options configuration
 module.exports.corsOrigins = [
   "http://localhost:8080",
+  "https://maruost.github.io",
   "https://maruost.github.io/news-frontend",
   "https://www.my-news-explorer.gq",
   "https://my-news-explorer.gq",
